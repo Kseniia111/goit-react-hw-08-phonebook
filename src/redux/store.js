c201a1fa43f983bcb07f2ad82bd71b8eef1fc6d5
Kseniia111 <kseniya.markovych@gmail.com>
2023-11-09 18:40:00 +0200

    auth start

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
 import contactsReducer from './ContactsSlice';
 import { filterReducer } from './FilterSlice';
+import { authReducer } from './auth/auth-slice';
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(persistConfig, authReducer),
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+// });
